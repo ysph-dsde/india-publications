6be7f414bde14bf5927e4c371173e19753515d43
Evan Cohen <evancohen19@gmail.com>
2025-09-11 14:12:20 -0400

    update ysph dsde logo

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -27,7 +27,7 @@ import {
 
 import { StatesSelector } from "./StatesSelector";
 import { useData } from "../../context/PublicationDataContext";
-import ysphLogo from "../../assets/images/ysphLogo.jpeg";
+import ysphLogo from "../../assets/images/ysphLogo.svg";
 import { Loading } from "./Loading";
 
 export const Sidebar = () => {
