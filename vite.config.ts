66cd6334e5d35f0a6dda569284631851db4579e2
Evan Cohen <evancohen19@gmail.com>
2025-09-25 15:32:55 -0400

    update packages to understand bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    visualizer({
+      filename: "./dist/stats.html", // Output file for the visualizer
+      open: true, // Automatically open the report in your browser after build
+    }),
+  ],
+});
