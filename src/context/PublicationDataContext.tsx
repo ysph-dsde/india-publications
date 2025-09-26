8b9498f67389d0b594300b9bb417ab523281df0c
Evan Cohen <evancohen19@gmail.com>
2025-09-26 17:37:25 -0400

    change default search

--- src/context/PublicationDataContext.tsx
+++ src/context/PublicationDataContext.tsx
@@ -103,10 +103,10 @@ export const DataProvider = ({ children }: { children: React.ReactNode }) => {
   // default filters
   const [serverFilters, setServerFilters] = useState<ServerFilters>({
     // topic: "Custom Keyword Search",
-    topic: "Digital Health",
+    topic: "Electronic Health Records",
     customKeyword: "",
-    // yearRange: [2014, 2024],
-    yearRange: [2014, 2018],
+    yearRange: [2014, 2024],
+    // yearRange: [2014, 2018],
   });
   const [clientFilters, setClientFilters] = useState<ClientFilters>({
     authorPosition: "First",
