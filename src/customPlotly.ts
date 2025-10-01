82882c013fecce41911b84b6d6ecac071126cfb7
Evan Cohen <evancohen19@gmail.com>
2025-10-01 15:09:33 -0400

    remove commented out previous version

--- src/customPlotly.ts
+++ src/customPlotly.ts
@@ -1,15 +1,3 @@
-// import PlotlyCore from "plotly.js/lib/core";
-
-// Promise.all([
-//   import("plotly.js/lib/bar"),
-//   import("plotly.js/lib/scatter"),
-//   import("plotly.js/lib/choropleth"),
-// ]).then(([Bar, Scatter, Choropleth]) => {
-//   PlotlyCore.register([Bar.default, Scatter.default, Choropleth.default]);
-// });
-
-// export default PlotlyCore;
-
 import PlotlyCore from "plotly.js/lib/core";
 import Bar from "plotly.js/lib/bar";
 import Scatter from "plotly.js/lib/scatter";
