929bf9cd28939ac2b038517d84e833ba20895e07
Evan Cohen <evancohen19@gmail.com>
2025-10-28 17:22:03 -0400

    add binary background image

--- src/components/LandingHero/LandingHero.tsx
+++ src/components/LandingHero/LandingHero.tsx
@@ -2,6 +2,7 @@ import Box from "@mui/material/Box";
 import { TopNav } from "./TopNav";
 import { Content } from "./Content";
 import type { PublicationTopic } from "../../constants/FilterTypes";
+import binary from "../../assets/images/binary.svg";
 
 export interface ExploreOption {
   label: PublicationTopic;
@@ -21,9 +22,29 @@ export const LandingHero = () => {
       sx={{
         width: "100%",
         background: "linear-gradient(to right, #000C8D, #790DA7)",
+        position: "relative",
         color: "#fff",
         borderBottomLeftRadius: 50,
         borderBottomRightRadius: 50,
+
+        // Semi-transparent image
+        "&::before": {
+          content: '""',
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          background: `url(${binary}) center/cover no-repeat`,
+
+          // Opacity gradient
+          maskImage: "linear-gradient(to right, transparent 10%, black 100%)",
+          maskSize: "100% 100%",
+          maskRepeat: "no-repeat",
+
+          // Base opacity (max opacity on the right)
+          opacity: 0.2,
+        },
       }}
     >
       <TopNav exploreOptions={exploreOptions} />
