5e8f85e569b15eeb2c4daa110a23525d67298b47
Evan Cohen <evancohen19@gmail.com>
2025-11-11 18:34:11 -0500

    update standard bottom margin for plots

--- src/components/MainContent/Plots/plotConfig.ts
+++ src/components/MainContent/Plots/plotConfig.ts
@@ -52,7 +52,7 @@ const baseLayout: Partial<Plotly.Layout> = {
       color: theme.palette.secondary.main,
     },
   },
-  margin: { t: 0, b: 50 },
+  margin: { t: 0, b: 70 },
 };
 
 // function to merge base layout with custom layout
